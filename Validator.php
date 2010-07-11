343a0d1c3d863b6f24ec22333638d51d37ecdd0f
Jeroen De Dauw <jeroendedauw@users.mediawiki.org>
2010-07-11 13:57:44 +0000

    Incremented version number

--- Validator.php
+++ Validator.php
@@ -24,7 +24,7 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 	die( 'Not an entry point.' );
 }
 
-define( 'Validator_VERSION', '0.3.4' );
+define( 'Validator_VERSION', '0.3.5 a1' );
 
 // Constants indicating the strictness of the parameter validation.
 define( 'Validator_ERRORS_NONE', 0 );
