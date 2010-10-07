45f55aca93093727ac640b66a5f3508f919a2e09
Jeroen De Dauw <jeroendedauw@users.mediawiki.org>
2010-10-07 21:54:38 +0000

    Added some docs

--- Validator_Settings.php
+++ Validator_Settings.php
@@ -21,7 +21,11 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 $wgHooks['ParserFirstCallInit'][] = 'ValidatorListErrors::staticInit';
 $wgHooks['LanguageGetMagic'][] = 'ValidatorListErrors::staticMagic';
 
-// TODO: document
+# Maps actions to error severity.
+# ACTION_LOG will cause the error to be logged
+# ACTION_WARN will cause a notice that there is an error to be shown
+# ACTION_SHOW will cause an error message to be shown
+# ACTION_DEMAND will cause an error and prevent rendering of the regular output
 $egErrorActions = array(
 	ValidationError::SEVERITY_MINOR => ValidationError::ACTION_LOG,
 	ValidationError::SEVERITY_LOW => ValidationError::ACTION_WARN,
