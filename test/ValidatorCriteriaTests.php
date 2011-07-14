54e8803aeba689ac96b893b23dff27bdb0901904
Jeroen De Dauw <jeroendedauw@users.mediawiki.org>
2011-07-14 20:56:31 +0000

    added test and option to have negative ints

--- test/ValidatorCriteriaTests.php
+++ test/ValidatorCriteriaTests.php
@@ -82,6 +82,9 @@ class ValidatorCriteriaTests extends MediaWikiTestCase {
 			array( true, '4.2', Parameter::TYPE_FLOAT, 0, 9 ),
 			array( true, '42', Parameter::TYPE_FLOAT, 0, 99 ),
 			array( false, '42', Parameter::TYPE_FLOAT, 0, 9 ),
+			array( true, '-42', Parameter::TYPE_INTEGER, false, 99 ),
+			array( true, '-42', Parameter::TYPE_INTEGER, -99, false ),
+			array( true, '42', Parameter::TYPE_INTEGER, -99, false ),
 		);
 		
 		foreach ( $tests as $test ) {
@@ -92,6 +95,64 @@ class ValidatorCriteriaTests extends MediaWikiTestCase {
 				$test[0],
 				$c->validate( $p, array() )->isValid(),
 				'Value "'. $test[1] . '" should ' . ( $test[0] ? '' : 'not ' ) . "be between '$test[3]' and '$test[4]'."
+			);
+		}
+	}
+	
+	/**
+	 * Tests CriterionIsFloat.
+	 */
+	public function testCriterionIsFloat() {
+		$tests = array(
+			array( true, '42' ),
+			array( true, '4.2' ),
+			array( false, '4.2.' ),
+			array( false, '42.' ),
+			array( false, '4a2' ),
+			array( true, '-42' ),
+			array( true, '-4.2' ),
+			array( false, '' ),
+			array( true, '0' ),
+			array( true, '0.0' ),
+		);
+		
+		foreach ( $tests as $test ) {
+			$c = new CriterionIsFloat();
+			$p = new Parameter( 'test' );
+			$p->setUserValue( 'test', $test[1] );
+			$this->assertEquals(
+				$test[0],
+				$c->validate( $p, array() )->isValid(),
+				'Value "'. $test[1] . '" should ' . ( $test[0] ? '' : 'not ' ) . "be a float."
+			);
+		}
+	}
+	
+	/**
+	 * Tests CriterionIsInteger.
+	 */
+	public function testCriterionIsInteger() {
+		$tests = array(
+			array( true, '42', true ),
+			array( false, '4.2', true ),
+			array( false, '4.2.', true ),
+			array( false, '42.', true ),
+			array( false, '4a2', true ),
+			array( true, '-42', true ),
+			array( false, '-42', false ),
+			array( false, '-4.2', true ),
+			array( false, '', true ),
+			array( true, '0', true ),
+		);
+		
+		foreach ( $tests as $test ) {
+			$c = new CriterionIsInteger( $test[2] );
+			$p = new Parameter( 'test' );
+			$p->setUserValue( 'test', $test[1] );
+			$this->assertEquals(
+				$test[0],
+				$c->validate( $p, array() )->isValid(),
+				'Value "'. $test[1] . '" should ' . ( $test[0] ? '' : 'not ' ) . "be an integer."
 			);
 		}
 	}
