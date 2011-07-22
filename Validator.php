97a1f8a3231b27483042f40186642f2733ddecfd
Jeroen De Dauw <jeroendedauw@users.mediawiki.org>
2011-07-22 21:16:39 +0000

    added message field to Parameter class so documentation hooks can properly handle multiple languages and incremented version number

--- Validator.php
+++ Validator.php
@@ -25,7 +25,7 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 	die( 'Not an entry point.' );
 }
 
-define( 'Validator_VERSION', '0.4.8' );
+define( 'Validator_VERSION', '0.4.9 alpha' );
 
 // Register the internationalization file.
 $wgExtensionMessagesFiles['Validator'] = dirname( __FILE__ ) . '/Validator.i18n.php';
