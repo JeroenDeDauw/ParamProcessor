2876d688e56e838bb7892ba89e519bf11df4b38f
Jeroen De Dauw <jeroendedauw@gmail.com>
2012-03-10 18:22:29 +0100

    get rid of changes

--- includes/definitions/CharParam.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class CharParam extends ParamDefinition {
-
-	public function validate( Param $param, array /* of Param */ $params ) {
-		return strlen( $param->getValue() ) === 1;
-	}
-
-}
--- includes/definitions/StringParam.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class StringParam extends ParamDefinition {
-
-	public function format( Param $param, array /* of Param */ $params ) {
-		$param->setValue( (string)$param->getValue() );
-	}
-
-}
