1757f08cd4744de5ad77afa2452e7882e6d7c46a
jeroendedauw <jeroendedauw@gmail.com>
2012-05-17 23:21:39 +0200

    work on dimension param
    Change-Id: I251f1cffdc73da102afd2befaf13f2cd573421df

---
+++ includes/definitions/DimensionParam.php
@@ -0,0 +1,96 @@
+<?php
+
+/**
+ * Defines the dimension parameter type.
+ * This parameter describes the size of a dimension (ie width) in some unit (ie px) or a percentage.
+ * Specifies the type specific validation and formatting logic.
+ *
+ * @since 0.5
+ *
+ * @file
+ * @ingroup Validator
+ * @ingroup ParamDefinition
+ *
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+class DimensionParam extends ParamDefinition {
+
+	/**
+	 * Returns an identifier for the parameter type.
+	 * @since 0.5
+	 * @return string
+	 */
+	public function getType() {
+		return 'dimension';
+	}
+
+	/**
+	 * Formats the parameter value to it's final result.
+	 *
+	 * @since 0.5
+	 *
+	 * @param $value mixed
+	 * @param $param iParam
+	 * @param $definitions array of iParamDefinition
+	 * @param $params array of iParam
+	 *
+	 * @return boolean
+	 */
+	protected function validateValue( $value, iParam $param, array $definitions, array $params ) {
+		if ( !parent::validateValue( $value, $param, $definitions, $params ) ) {
+			return false;
+		}
+
+		if ( !$this->canBeEmpty && $value === '' ) {
+			return false;
+		}
+
+		if ( $this->length !== false ) {
+			$length = strlen( $value );
+
+			if ( is_array( $this->length ) ) {
+				return ( $this->length[1] === false || $value <= $this->length[1] )
+					&& ( $this->length[0] === false || $value >= $this->length[0] );
+			}
+			else {
+				return $length == $this->length;
+			}
+		}
+	}
+
+	/**
+	 * Formats the parameter value to it's final result.
+	 *
+	 * @since 0.5
+	 *
+	 * @param $value mixed
+	 * @param $param iParam
+	 * @param $definitions array of iParamDefinition
+	 * @param $params array of iParam
+	 *
+	 * @return mixed
+	 */
+	protected function formatValue( $value, iParam $param, array &$definitions, array $params ) {
+		$value = (string)$value;
+
+		if ( $this->toLower ) {
+			$value = strtolower( $value );
+		}
+
+		return $value;
+	}
+
+	/**
+	 * Sets the parameter definition values contained in the provided array.
+	 *
+	 * @since 0.5
+	 *
+	 * @param array $param
+	 */
+	public function setArrayValues( array $param ) {
+		parent::setArrayValues( $param );
+
+	}
+
+}
