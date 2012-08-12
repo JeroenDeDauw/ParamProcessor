6da736c6c250b78b0322bb29e9141eaf774f4458
jeroendedauw <jeroendedauw@gmail.com>
2012-08-12 19:24:30 +0200

    Added tests for ValidatorOptions
    Change-Id: I3ddd4fec7bc69c2c4cb412189145345c64b592e5

--- tests/ValidatorOptionsTest.php
+++ tests/ValidatorOptionsTest.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Validator\Test;
+use ValidatorOptions;
 
 /**
  * Unit test for the Validator\Options class.
@@ -19,5 +20,44 @@ namespace Validator\Test;
  */
 class ValidatorOptionsTest extends \MediaWikiTestCase {
 
+	public function testConstructor() {
+		$this->assertInstanceOf( '\ValidatorOptions', new ValidatorOptions() );
+	}
+
+	/**
+	 * @return \ValidatorOptions
+	 */
+	protected function getInstance() {
+		return new ValidatorOptions();
+	}
+
+	public function testBooleanSettersAndGetters() {
+		$methods = array(
+			'setUnknownInvalid' => 'getUnknownInvalid',
+			'setLowercaseNames' => 'getLowercaseNames',
+			'setRawStringInputs' => 'getRawStringInputs',
+			'setTrimValues' => 'getTrimValues',
+			'setLowercaseValues' => 'getLowercaseValues',
+		);
+
+		foreach ( $methods as $setter => $getter ) {
+			$options = $this->getInstance();
+
+			foreach ( array( false, true, false ) as $boolean ) {
+				call_user_func_array( array( $options, $setter ), array( $boolean ) );
+
+				$this->assertEquals( $boolean, call_user_func( array( $options, $getter ) ) );
+			}
+		}
+	}
+
+	public function testSetAndGetName() {
+		$options = $this->getInstance();
+
+		foreach ( array( 'foo', 'bar baz' ) as $name ) {
+			$options->setName( $name );
+			$this->assertEquals( $name, $options->getName() );
+		}
+	}
 
 }
