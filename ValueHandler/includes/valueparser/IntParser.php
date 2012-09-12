0b139c09f9f7008a2971167059404624a337e1c0
jeroendedauw <jeroendedauw@gmail.com>
2012-09-12 17:34:33 +0200

    Added int and float parsers
    Change-Id: Iaa12e355398bb5bac52a41bbbe9f67e014db5b2c

--- ValueHandler/includes/valueparser/IntParser.php
+++ ValueHandler/includes/valueparser/IntParser.php
@@ -1,8 +1,52 @@
 <?php
+
 /**
- * Created by JetBrains PhpStorm.
- * User: jeroen
- * Date: 9/5/12
- * Time: 3:58 PM
- * To change this template use File | Settings | File Templates.
+ * ValueParser that parses the string representation of an integer.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @since 0.1
+ *
+ * @file
+ * @ingroup ValueHandler
+ * @ingroup ValueParser
+ *
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
  */
+class IntParser extends StringValueParser {
+
+	/**
+	 * @see StringValueParser::stringParse
+	 *
+	 * @since 0.1
+	 *
+	 * @param string $value
+	 *
+	 * @return ValueParserResult
+	 */
+	public function stringParse( $value ) {
+		$positiveValue = strpos( $value, '-' ) === 0 ? substr( $value, 1 ) : $value;
+
+		if ( ctype_digit( $positiveValue ) ) {
+			return ValueParserResultObject::newSuccess( (int)$value );
+		}
+		else {
+			return ValueParserResultObject::newErrorText( 'Not an integer' );
+		}
+	}
+
+}
