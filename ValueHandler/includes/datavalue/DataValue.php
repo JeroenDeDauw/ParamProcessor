a5cb27bd7d8f988e76793a5d96ee0c3c693c5b1c
jeroendedauw <jeroendedauw@gmail.com>
2012-09-13 23:09:11 +0200

    Added datavalue files
    Change-Id: I8252040feb7a40ee665d0144ba969990333f3958

---
+++ ValueHandler/includes/datavalue/DataValue.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace DataValue;
+
+interface Hashable {
+
+	public function getHash();
+
+}
+
+interface Comparable {
+
+	public function equals( $dataValue );
+
+}
+
+/**
+ * Interface for objects that represent a single data value.
+ *
+ * @since 0.1
+ *
+ * @file
+ * @ingroup DataValue
+ *
+ * @licence GNU GPL v2+
+ * @author Jeroen De Dauw < jeroendedauw@gmail.com >
+ */
+interface DataValue extends Hashable, Comparable, \Serializable {
+
+	public function getType();
+
+	public function getSortKey();
+
+}
