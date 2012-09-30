827976d638e2625ef14691fb19018ddafc2a7a97
jeroendedauw <jeroendedauw@gmail.com>
2012-09-30 19:52:46 +0200

    Work on TitleValidator
    Change-Id: I18d01412ca645824188c4b77b3e0878b65844886

--- ValueHandler/includes/valuevalidator/TitleValidator.php
+++ ValueHandler/includes/valuevalidator/TitleValidator.php
@@ -37,10 +37,32 @@ class TitleValidator extends ValueValidatorObject {
 
 	/**
 	 * @since 0.1
+	 * @var array
+	 */
+	protected $namespaces = array();
+
+	/**
+	 * Sets if there needs to be an article that has this title.
+	 *
+	 * @since 0.1
+	 *
 	 * @param boolean $hasToExist
 	 */
 	public function setHasToExist( $hasToExist ) {
 		$this->hasToExist = $hasToExist;
+	}
+
+	/**
+	 * Sets a whitelist of namespaces, ie a list into which the
+	 * namespace of the title needs to be. You can provide an
+	 * array of namespace integers or a single namespace int.
+	 *
+	 * @since 0.1
+	 *
+	 * @param array|int $namespaces
+	 */
+	public function setNamespaceRestriction( $namespaces ) {
+		$this->namespaces = (array)$namespaces;
 	}
 
 	/**
@@ -57,8 +79,14 @@ class TitleValidator extends ValueValidatorObject {
 		if ( !$value instanceof Title ) {
 			$this->addErrorMessage( 'Not a title' );
 		}
-		elseif( $this->hasToExist && !$value->exists() ) {
-			$this->addErrorMessage( 'Title does not exist' );
+		else {
+			if ( $this->hasToExist && !$value->exists() ) {
+				$this->addErrorMessage( 'Title does not exist' );
+			}
+
+			if ( $this->namespaces !== array() && !in_array( $value->getNamespace(), $this->namespaces ) ) {
+				$this->addErrorMessage( 'Title not in the required namespace' );
+			}
 		}
 	}
 
@@ -74,6 +102,10 @@ class TitleValidator extends ValueValidatorObject {
 
 		if ( array_key_exists( 'hastoexist', $options ) ) {
 			$this->setHasToExist( $options['hastoexist'] );
+		}
+
+		if ( array_key_exists( 'namespaces', $options ) ) {
+			$this->setNamespaceRestriction( $options['namespaces'] );
 		}
 	}
 
