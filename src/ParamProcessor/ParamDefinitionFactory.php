c7f4dc804eee74be52685891b7e5d1a03ba6036f
jeroendedauw <jeroendedauw@gmail.com>
2013-11-29 00:36:34 +0100

    Rem obsolete doc

--- src/ParamProcessor/ParamDefinitionFactory.php
+++ src/ParamProcessor/ParamDefinitionFactory.php
@@ -8,8 +8,6 @@ use OutOfBoundsException;
 /**
  * Factory for IParamDefinition implementing objects.
  *
- * @ingroup ParamProcessor
- *
  * @licence GNU GPL v2+
  * @author Jeroen De Dauw < jeroendedauw@gmail.com >
  */
