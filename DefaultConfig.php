bb86579092bd8e1d0531599a4a3c8e9efba57660
jeroendedauw <jeroendedauw@gmail.com>
2016-05-24 12:51:18 +0200

    Switched config to short array syntax

--- DefaultConfig.php
+++ DefaultConfig.php
@@ -6,33 +6,33 @@
  * @licence GNU GPL v2+
  */
 
-$GLOBALS['egValidatorSettings'] = array(
+$GLOBALS['egValidatorSettings'] = [
 	'errorListMinSeverity' => 'minor',
-);
+];
 
-$GLOBALS['wgParamDefinitions'] = array(
-	'boolean' => array(
+$GLOBALS['wgParamDefinitions'] = [
+	'boolean' => [
 		'string-parser' => '\ValueParsers\BoolParser',
 		'validation-callback' => 'is_bool',
-	),
-	'float' => array(
+	],
+	'float' => [
 		'string-parser' => '\ValueParsers\FloatParser',
 		'validation-callback' => function( $value ) {
 			return is_float( $value ) || is_int( $value );
 		},
 		'validator' => '\ValueValidators\RangeValidator',
-	),
-	'integer' => array(
+	],
+	'integer' => [
 		'string-parser' => '\ValueParsers\IntParser',
 		'validation-callback' => 'is_int',
 		'validator' => '\ValueValidators\RangeValidator',
-	),
-	'string' => array(
+	],
+	'string' => [
 		'validator' => '\ValueValidators\StringValidator',
 		'definition' => '\ParamProcessor\Definition\StringParam',
-	),
-	'dimension' => array(
+	],
+	'dimension' => [
 		'definition' => '\ParamProcessor\Definition\DimensionParam',
 		'validator' => '\ValueValidators\DimensionValidator',
-	),
-);
+	],
+];
